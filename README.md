fc9097892375b9fed6ad3bb268103668187e93c4
Jon Borgonia <jon@gomagames.com>
2014-10-09 00:02:03 -1000

    adds initial HaxePunk->webgl target with obligatory hello world

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Be-Positive
 ===========
 
 simulation using the rules for valid blood tranfusions
+
+## Uses HaxePunk for webgl rendering
---
+++ src/MainScene.hx
@@ -0,0 +1,10 @@
+import com.haxepunk.Scene;
+
+class MainScene extends Scene
+{
+	public override function begin()
+	{
+    trace("Hello Console");
+    add( new com.haxepunk.Entity( 100, 100, new com.haxepunk.graphics.Text("Hello World")));
+	}
+}
