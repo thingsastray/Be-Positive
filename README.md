4fb643e8cdad7422925d78fe8e70c2bcea5404aa
Jon Borgonia <jon@gomagames.com>
2014-10-09 01:01:32 -1000

    adds haxe->gulpfile.js for running server with livereload

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ Be-Positive
 
 simulation using the rules for valid blood tranfusions
 
-## Uses HaxePunk for webgl rendering
+### Uses HaxePunk for webgl rendering
+
+## Run using gulp task
+`gulp`
+
+work in public directory `bin/html5/`
---
+++ task/Gulpfile.hx
@@ -0,0 +1,62 @@
+/*
+  Gulp task
+
+  this is used to create the gulpfile.js
+
+  tasks:
+    - default : runs http server with live reload
+    - test : todo 
+*/
+import js.Node;
+
+typedef Gulp = {
+  task : String->Dynamic->Void,
+  watch : Array<String>->(GulpEvent->Void)->Void,
+  src : String->Dynamic
+}
+
+typedef GulpEvent = {
+  type : String,
+  path : String
+}
+
+typedef GulpConnect = {
+  server : Dynamic->Void,
+  reload : Void->Void
+}
+
+class Gulpfile
+{
+  public static inline var WEBROOT:String = "./bin/html5";
+
+  private static var gulp:Gulp = Node.require("gulp");
+  private static var connect:GulpConnect = Node.require("gulp-connect");
+  
+  public static function main()
+  {
+    gulp.task('default', ['connect','watch']);
+    gulp.task('watch',  watch_task);
+    gulp.task('connect', connect_task);
+  }
+
+
+  private static inline function connect_task(cb:Void->Void):Void
+  {
+    connect.server({
+      root: WEBROOT,
+      livereload: true
+    });
+  }
+
+  private static inline function watch_task(?cb:Void->Void):Void
+  {
+    gulp.watch(['$WEBROOT/**'], reload);
+  }
+
+  private static inline function reload(event:GulpEvent):Void
+  {
+    gulp.src('$WEBROOT/**')
+      .pipe(connect.reload());
+  }
+  
+}
