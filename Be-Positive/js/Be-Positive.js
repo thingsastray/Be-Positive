97db7d5646aa136842197adb2ac95afed5c8e39f
thingsastray <andrewpeterson808@gmail.com>
2015-11-28 15:14:20 -1000

    Second

--- Be-Positive/js/Be-Positive.js
+++ Be-Positive/js/Be-Positive.js
@@ -46,116 +46,54 @@ BloodTransfusionRules = {
    */
 
   receive_patient : function (blood_inventory, patient) {
-
+      var acceptableBlood = [];
       if (patient.blood_type === BloodType.AB_POS) {
-        if (blood_inventory.AB_POS > 0){
-          return BloodType.AB_POS;
-        }
-        if (blood_inventory.AB_NEG > 0){
-          return BloodType.AB_NEG;
-        }
-        if (blood_inventory.A_POS > 0){
-          return BloodType.A_POS;
-        }
-        if (blood_inventory.A_NEG > 0){
-          return BloodType.A_NEG;
-        }
-        if (blood_inventory.B_POS > 0){
-          return BloodType.B_POS;
-        }
-        if (blood_inventory.B_NEG > 0){
-          return BloodType.B_NEG;
-        }
-        if (blood_inventory.O_POS > 0){
-          return BloodType.O_POS;
-        }
-        if (blood_inventory.O_NEG > 0){
-          return BloodType.O_NEG;
-        }
-        return false;            
+        acceptableBlood = ["AB_POS", "AB_NEG", "A_POS", "A_NEG","B_POS", "B_NEG", "O_POS", "O_NEG"];
+        return searchBloodtype(blood_inventory, acceptableBlood);            
       }
 
       if (patient.blood_type === BloodType.AB_NEG) {
-        if (blood_inventory.AB_NEG > 0){
-          return BloodType.AB_NEG;
-        }
-        if (blood_inventory.A_NEG > 0){
-          return BloodType.A_NEG;
-        }
-        if (blood_inventory.B_NEG > 0){
-          return BloodType.B_NEG;
-        }
-        if (blood_inventory.O_NEG > 0){
-          return BloodType.O_NEG;
-        }
-        return false;
+        acceptableBlood = ["AB_NEG", "A_NEG", "B_NEG", "O_NEG"];
+        return searchBloodtype(blood_inventory, acceptableBlood);
       }
 
       if (patient.blood_type === BloodType.A_NEG) {
-        if (blood_inventory.A_NEG > 0){
-          return BloodType.A_NEG;
-        }
-        if (blood_inventory.O_NEG > 0){
-          return BloodType.O_NEG;
-        }
-        return false;
+        acceptableBlood = ["A_NEG", "O_NEG"];
+        return searchBloodtype(blood_inventory, acceptableBlood);
       }
 
       if (patient.blood_type === BloodType.A_POS) {
-        if (blood_inventory.A_POS > 0){
-          return BloodType.A_POS;
-        }
-        if (blood_inventory.A_NEG > 0){
-          return BloodType.A_NEG;
-        }
-        if (blood_inventory.O_POS > 0){
-          return BloodType.O_POS;
-        }
-        if (blood_inventory.O_NEG > 0){
-          return BloodType.O_NEG;
-        }
-        return false;
+        acceptableBlood = ["A_POS", "A_NEG", "O_POS", "O_NEG"];
+        return searchBloodtype(blood_inventory, acceptableBlood);
       }
 
       if (patient.blood_type === BloodType.B_POS) {
-        if (blood_inventory.B_POS > 0){
-          return BloodType.B_POS;
-        }
-        if (blood_inventory.B_NEG > 0){
-          return BloodType.B_NEG;
-        }
-        if (blood_inventory.O_POS > 0){
-          return BloodType.O_POS;
-        }
-        if (blood_inventory.O_NEG > 0){
-          return BloodType.O_NEG;
-        }
-        return false; 
+      acceptableBlood = ["B_POS", "B_NEG", "O_POS", "O_NEG"];
+        return searchBloodtype(blood_inventory, acceptableBlood);
       }
 
       if (patient.blood_type === BloodType.B_NEG){
-        if (blood_inventory.B_NEG > 0){
-          return BloodType.B_NEG;
-        }
-        if (blood_inventory.O_NEG > 0){
-          return BloodType.O_NEG;
-        }
+acceptableBlood = ["B_NEG", "O_NEG"];
+        return searchBloodtype(blood_inventory, acceptableBlood);
       }
       if (patient.blood_type === BloodType.O_NEG){
-        if (blood_inventory.O_NEG > 0){
-          return BloodType.O_NEG;
-        }
-        return false; 
+        acceptableBlood = ["O_NEG"];
+        return searchBloodtype(blood_inventory, acceptableBlood); 
       }
 
       if (patient.blood_type === BloodType.O_POS){
-        if (blood_inventory.O_POS > 0){
-          return BloodType.O_POS;
-        }
-        if (blood_inventory.O_NEG > 0){
-          return BloodType.O_NEG;
-        }
-        return false;
+        acceptableBlood = ["O_POS", "O_NEG"];
+        return searchBloodtype(blood_inventory, acceptableBlood);
       }
     }
 };
+
+function searchBloodtype (blood_inventory, acceptableBlood) {
+    for (var i = 0; i < acceptableBlood.length; i++) {
+    var bloodType = acceptableBlood[i];
+    if (blood_inventory[bloodType] > 0){
+      return BloodType[bloodType];
+    }
+  }
+  return false; 
+}
